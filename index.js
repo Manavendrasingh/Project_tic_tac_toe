c30bf7b8638952b6f36cdcb84a2a712b6d3bbb4e
manavendrajss@gmail.com <manavendrajss@gmail.com>
2024-06-20 17:21:21 +0530

    new version

--- index.js
+++ index.js
@@ -1,51 +1,99 @@
 
 let boxs = document.querySelectorAll(".box");
 let game = document.querySelector(".game");
-let reset = document.querySelector("#reset");
+let start = document.querySelector("#reset");
 let msg = document.querySelector(".msg");
-let OO = document.querySelector("#OO");
-let XX = document.querySelector("#XX");
 
 
 
-reset.addEventListener("click",()=>{
-    for(box of boxs) 
+
+let who = "O";
+let count =0;
+
+let clickSound = new Audio('./soundEffects/click.mp3')
+let startSound = new Audio('./soundEffects/Start.wav')
+let winSound = new Audio('./soundEffects/win.wav')
+
+
+
+
+const  disableRestBox = () =>{
+    for(b of boxs)
         {
-            box.disabled = false;
-            box.innerHTML = "";
-            msg.innerHTML = "";
-            XX.value = "";
-            OO.value = "";
+            b.disabled = true;
         }
+    
+   };
 
+disableRestBox();
+
+const startGame = ()=>{
+            count =0;
+            getSoundEffect("start");
+            setTimeout(()=>{
+            for(b of boxs) 
+                {
+                    b.disabled = false;
+                    b.innerHTML = "";
+                    msg.innerHTML = `Start With : ${who}`;
+                    
+                }},3000)    
+}
+
+start.addEventListener("click",()=>{
+    count = 0;
+    getSoundEffect("start");
+    for(b of boxs) 
+        {
+            b.disabled = false;
+            b.innerHTML = "";
+            msg.innerHTML = `Start With : ${who}`;
+            
+        }
 })
-let ternO = true;
+
+
+const getSoundEffect = (sound)=>{
+   if(sound === "start") startSound.play();
+   else if(sound === "win") winSound.play();
+   else if(sound === "click") clickSound.play();
+
+}
+
+
 const winPattern = [[1,2,3],[1,4,7],[1,5,9],
            [4,5,6],[7,8,9],[7,5,3],
            [2,5,8],[3,6,9]];
 
-          // boxs.forEach((box) => { normal use of event listner (1)
-                game.addEventListener("click", (event)=>{ // event deligation (2)
-                    if(ternO)
-                        {
+          // boxs.forEach((box) => { normal use of event listner (1
 
-// console.dir(event); 
-// console.dir(event.srcElement);
-// console.dir(event.srcElement.outHTML);                          
-                                    event.target.innerHTML= "O";
-                                    ternO = false;
-                                         
+                game.addEventListener("click", (event)=>{ // event deligation (2)
+                    getSoundEffect("click");
+                    count++;
+                    //console.log(count);
+                    if(count === 9)
+                         {    
+                               msg.innerHTML = "No one won this match better luck next time";
+
+                               startGame();
+                         }
+                    if(who === "O")
+                        {                        
+                                event.target.innerHTML= "O";
+                                who = 'X';
+                                            
                         }
-                    else{
-                        
+                        else{
+                            
                                 event.target.innerHTML = "X";
-                                ternO = true;         
-                        
-                    }    
-                    event.target.disabled=true;
-                    checkWinner();
-                },true);
-            //});
+                                who = 'O';         
+                            
+                        }    
+                        event.target.disabled=true;
+                        checkWinner();
+                });
+           // });
+
             function checkWinner(){
                     for(pattern of winPattern)
                         {
@@ -60,24 +108,24 @@ const winPattern = [[1,2,3],[1,4,7],[1,5,9],
                                         {
                                             if(pos1 == "O")
                                                 {
-                                                    msg.innerHTML = `!congratulation ${OO.value ? OO.value + " - O": "O"} is the winner`;
+                                                    msg.innerHTML = `!congratulation O is the winner`;
+                                                   
                                                 }
                                             else{
-                                                msg.innerHTML = `!congratulation ${XX.value ? XX.value + " - X": "X"} is the winner`;
-                                            }    
+                                                    msg.innerHTML = `!congratulation X is the winner`;
+                                                    
+                                            }
+                                            getSoundEffect("win");   
                                             disableRestBox();
+                                            startGame();
                                             
                                         }
+                                        
                                 }
+                                
                         }
             }
-            
-           const disableRestBox = ()=>{
-            for(box of boxs)
-                {
-                    box.disabled = true;
-                }
-           }
+        
 
 
 
